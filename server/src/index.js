c6a7e1ee11b766528b284d1e4abcd3d1de4ce3f4
f-lyp3 <filipy7lype@gmail.com>
2020-08-26 20:13:10 +0100

    move routes logic to another file, and import that file

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 
+const routes = require("./routes");
+
 const app = express();
 
-app.get("/", (req, res) => {
-    return res.json({
-        evento: "Semana omnistack 11.0",
-        aluno: "iFilipe Lype"
-    });
-})
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
