8ed051e5722ef8f940c4c275c4d54b841831c391
f-lyp3 <filipy7lype@gmail.com>
2020-08-26 23:19:52 +0100

    add cors functionality

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 
 const routes = require("./routes");
 
 const app = express();
 
+// allow the app to exchange data through JSON
 app.use(express.json());
+
+// allow all request from others client, development only.
+app.use(cors());
+
+// setting up routes
 app.use(routes);
 
 app.listen(3333);
