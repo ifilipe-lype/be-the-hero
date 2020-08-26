6480ceb3139d5e56249fcbe6ea2f61dc59011994
f-lyp3 <filipy7lype@gmail.com>
2020-08-26 21:58:33 +0100

    create ong-controller

---
+++ server/src/controllers/OngController.js
@@ -0,0 +1,24 @@
+const crypto = require("crypto");
+const db = require("../database");
+
+module.exports = {
+    async index(req, res){
+        // get all ongs from database
+        const ongs = await db("ongs").select("*");
+        return res.json(ongs);
+    }
+    ,
+    async create(req, res) {
+        const { name, email, whatsapp, city, uf } = req.body;
+    
+        // generate a random hex string. will it be unique in long-term ?
+        const id = crypto.randomBytes(4).toString("hex");
+    
+        // one ong to the database, on table ongs.
+        await db("ongs").insert({
+            id, name, email, whatsapp, city, uf
+        });
+    
+        return res.json({ id });
+    }
+}
