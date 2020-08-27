ddd653a3550d8c1a9c411888be19f724212893a3
f-lyp3 <filipy7lype@gmail.com>
2020-08-27 22:16:23 +0100

    create header structure

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiPower } from "react-icons/fi";
+
+import logoImg from "../../assets/logo.svg"
 
 import "./style.css";
 
 export default function Profile(){
     return (
-        <h1>Profile page</h1>
+        <div className="profile-container">
+            <header>
+                <img src={logoImg} alt="Be The Hero"/>
+                <span>Bem vindo, ONU</span>
+
+                <Link to="/incidents/new" className="button">
+                    Cadastrar novo incidente
+                </Link>
+                <button>
+                    <FiPower size={18} color="#E02041" />
+                </button>
+            </header>
+        </div>
     )
 }
 
