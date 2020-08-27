b3aac98581a6bba7a93bdb77fe179303af02b746
f-lyp3 <filipy7lype@gmail.com>
2020-08-27 21:19:34 +0100

    change anchor html tag for Link component

--- web/src/pages/Logon/index.js
+++ web/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FiLogIn } from "react-icons/fi";
 
 import "./style.css";
@@ -19,10 +20,10 @@ export default function Logon(){
                         Entrar
                     </button>
 
-                    <a href="/register">
+                    <Link to="/register">
                         <FiLogIn size={16} color="#E02041"/>
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
             <img src={heroesImg} alt="heroes" />
