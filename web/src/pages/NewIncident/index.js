67333ed5f012dfb15f6d5daa3cb629ade1ceedda
f-lyp3 <filipy7lype@gmail.com>
2020-08-28 01:42:43 +0100

    remove unused variables

--- web/src/pages/NewIncident/index.js
+++ web/src/pages/NewIncident/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 
@@ -23,7 +23,7 @@ export default function NewIncident(){
             value
         }
         try {
-            const response = await api.post("incidents", data, {
+            await api.post("incidents", data, {
                 headers: {
                     Authorization: ongID
                 }
