3c5738595a52a3552473977ac0281cd4706e517e
f-lyp3 <filipy7lype@gmail.com>
2020-08-29 21:00:55 +0100

    create header section

--- mobile/src/pages/Incidents/styles.js
+++ mobile/src/pages/Incidents/styles.js
@@ -1,5 +1,34 @@
 import { StyleSheet } from "react-native";
+import Constants from "expo-constants";
 
 export default StyleSheet.create({
-    container: {}
+    container: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: Constants.statusBarHeight + 20
+    },
+    header: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    headerText: {
+        fontSize: 15,
+        color: "#717180"
+    },
+    headerTextBold: {
+        fontWeight: "bold"
+    },
+    title: {
+        fontSize: 30,
+        color: "#13131a",
+        marginTop: 40,
+        marginBottom: 16,
+        fontWeight: "bold"
+    },
+    description: {
+        fontSize: 16,
+        lineHeight: 24,
+        color: "#737380"
+    }
 });
